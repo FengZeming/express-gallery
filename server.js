5bee75f86e46310310fefcfae33213815d05af03
Alexander Anich <alexander.anich@gmail.com>
2015-10-20 11:27:56 -1000

    create listing generator mixin && hard input image links

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.set('views', './views');
 app.use(express.static('./public'));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {
+    listings : ['http://www.wired.com/wp-content/uploads/2015/01/Sense-of-Place_Mountain-Dwellings_Photograph-by-Pawel-Paniczko.jpg',
+                'http://www.wired.com/wp-content/uploads/2015/02/Exteriors_The-Ivar-Aasen-Centre_Photograph-by-David-Borland-682x1024.jpg',
+                'http://www.wired.com/wp-content/uploads/2015/02/Exteriors_Power-Transformation-Station_Pgotograph-by-Tim-Van-de-Velde.jpg']
+  });
 });
 
 app.use('/gallery', gallery);
